43e89b687b3bbbc679aaa37e0dd6edb8e1565e69
Erik Rimskog <erik.rimskog@gmail.com>
2018-11-28 15:57:30 +0100

    two test functions

--- src/client_main.rs
+++ src/client_main.rs
@@ -2,13 +2,61 @@
 extern crate peas_rf_cp;
 
 use peas_rf_cp::common::logger;
+use peas_rf_cp::tracker::api;
+use peas_rf_cp::network::{self,NetworkError};
+use std::env;
+use std::net::ToSocketAddrs;
+use peas_rf_cp::common::id::Id;
 
 fn main() {
-    logger::init(logger::LevelFilter::Debug);
-    log::debug!("Hello, world!");
+    // logger::init(logger::LevelFilter::Debug);
+    // log::debug!("Hello, world!");
 
-    println!("main started");
-    // magic goes here
+    // uppdatera oss själva för att sedan lista alla som tracker vet om
 
-    log::debug!("Shutting down application...");
+    let args: Vec<_> = env::args().collect();
+    let sock = network::udp::open_any().unwrap();
+    let tracker = args[1].to_socket_addrs().unwrap().next().unwrap();
+
+    println!("tracker: {:?}", tracker);
+    println!("me: {:?}", sock.local_addr().unwrap());
+
+    // update test_id on tracker
+    let test_id = Id::from_u64(12);
+    println!("updating {}", test_id);
+    let dur = match api::update(&sock, test_id, sock.local_addr().unwrap(), tracker) {
+        Err(NetworkError::Timeout) => {
+            println!("tracker not responding :(");
+            std::process::exit(1);
+        },
+        Err(_) => {
+            println!("shshzhshshszzhzzsh");
+            std::process::exit(1);
+        },
+        Ok(ok) => ok,
+    };
+    println!("{} should now stay there for {}s", test_id, dur.as_secs());
+
+    // querying everything
+    let mut ls = api::LookupSession::new(&sock, tracker, test_id);
+
+    loop {
+        let cur = ls.next();
+        match cur {
+            Err(NetworkError::Timeout) => {
+                println!("tracker not responding");
+                break;
+            },
+            Err(_) => {
+                println!("ööööh");
+                break;
+            },
+            Ok(None) => break,
+            Ok(Some(adr)) => {
+                println!("{} knows about {}", adr, test_id);
+            }
+        }
+    }
+
+    // log::debug!("Shutting down application...");
 }
--- src/tracker_main.rs
+++ src/tracker_main.rs
@@ -1,73 +1,10 @@
 
 extern crate peas_rf_cp;
 
-#[macro_use]
-extern crate serde_derive;
-extern crate serde;
-extern crate bincode;
-
 use std::net::{UdpSocket,SocketAddr};
 use std::env;
-use bincode::{serialize, deserialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Wow {
-    name: String,
-}
+use peas_rf_cp::tracker::server;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() <= 1 {
-        eprintln!("Usage: tracker (--receiver | --sender)");
-        std::process::exit(1);
-    }
-
-    let sen = SocketAddr::from(([127,0,0,1], 8081));
-    let rec = SocketAddr::from(([127,0,0,1], 8080));
-    if args[1] == "--receiver" {
-        receiver(rec);
-    } else if args[1] == "--sender" {
-        sender(sen, rec);
-    }
+    server::start(12345, 600);
 }
-
-fn sender(myself: SocketAddr, target: SocketAddr) {
-    println!("sender started! I am {}", myself);
-
-    let socket = UdpSocket::bind(SocketAddr::from(myself)).unwrap();
-
-    let wow = Wow{ name: "omg".to_string() };
-
-    let seri = serialize(&wow).unwrap();
-
-    println!("I am sending {:?}", wow);
-
-    socket.send_to(&seri, target).unwrap();
-    println!("i sent some stuff to {}! (it hopefully arrived)", target);
-
-}
-
-fn receiver(myself: SocketAddr) {
-    println!("tracker started! I am {}", myself);
-
-    let socket = UdpSocket::bind(SocketAddr::from(myself)).unwrap();
-
-    let mut buffer = [0; 1024];
-
-    // println!("sleeping...");
-    // std::thread::sleep(std::time::Duration::from_millis(10000));
-
-    println!("receiving...");
-    let (_amount, src) = socket.recv_from(&mut buffer).unwrap();
-
-    println!("received from {}", src);
-
-    let recv: Wow;
-    match deserialize(&buffer) {
-        Ok(w) => recv = w,
-        Err(_) => {println!("received garbage :("); return},
-    }
-
-    println!("{:?}", recv);
-}
-
