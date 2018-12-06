cd640a3dc5bb986f2a92c72f5eb006de07f55404
Staffan Annerwall <staffan.annerwall.3984@student.uu.se>
2018-12-06 13:04:57 +0100

    Fix compilation error in tracker

--- src/common/logger.rs
+++ src/common/logger.rs
@@ -3,7 +3,7 @@ use log::{self, LevelFilter};
 
 pub fn initialize_logger(level: LevelFilter, to_file: bool) {
     if level != LevelFilter::Off {
-        let logger = Logger::with(LogSpecification::default(level).build())
+        Logger::with(LogSpecification::default(level).build())
             .o_log_to_file(to_file)
             .format(flexi_logger::opt_format)
             .start()
